581adddd1eb277e06947a306284e58c0330d807c
moizes86 <moshe.mn86@gmail.com>
2020-10-27 14:05:46 +0200

    product-item is responsive

--- src/components/product-item/product-item.component.jsx
+++ src/components/product-item/product-item.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./product-item.styles.scss";
 
-import CustomButton from '../custom-button/custom-button.component';
+import CustomButton from "../custom-button/custom-button.component";
 
 const ProductItem = ({ description, image, price, title, type }) => {
   return (
@@ -11,11 +11,13 @@ const ProductItem = ({ description, image, price, title, type }) => {
         style={{ backgroundImage: `url(${image})` }}
       ></div>
       <div className="content">
-        <h3 className="title">{title}</h3>
-        <span className="description">{description} </span>
+        <div className="details">
+          <h3 className="title">{title}</h3>
+          <span className="description">{description} </span>
+        </div>
         <div className="price-and-btn">
           <span className="price">{price}$</span>
-          <CustomButton > ADD TO CART </CustomButton>
+          <CustomButton> ADD TO CART </CustomButton>
         </div>
       </div>
     </div>
